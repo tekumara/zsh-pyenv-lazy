69cc08a112dd37829bac044c6623d27b324ccb21
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-06-08 20:23:55 +1000

    pyenv 2.0.0 now requires setting the path
    see https://github.com/pyenv/pyenv/issues/1959

--- pyenv-vew-lazy.plugin.zsh
+++ pyenv-vew-lazy.plugin.zsh
@@ -1,24 +1,26 @@
-#!/bin/sh	
+#!/bin/sh
 
-# pyenv can't be lazy loaded using the same technique for 
+# pyenv can't be lazy loaded using the same technique for
 # virtualenvwrapper because if a virtualenv is activated before
 # pyenv loads (eg: by an IDE terminal) it will break the virtualenv
+
+export PATH="${HOME}/.pyenv/shims:${PATH}"
 eval "$(pyenv init - --no-rehash zsh)"
 
 # lazy load virtualenvwrapper for faster shell startup times
 # faster than pyenv virtualenvwrapper_lazy
 __vew_init() {
-    pyenv virtualenvwrapper
-}	
+	pyenv virtualenvwrapper
+}
 
 mkvirtualenv() {
-	__vew_init	
-	mkvirtualenv "$@"	
+	__vew_init
+	mkvirtualenv "$@"
 }
 
 workon() {
-	__vew_init	
-	workon "$@"	
+	__vew_init
+	workon "$@"
 }
 
 mktmpenv() {
@@ -27,6 +29,6 @@ mktmpenv() {
 }
 
 rmvirtualenv() {
-	__vew_init	
-	rmvirtualenv "$@"	
+	__vew_init
+	rmvirtualenv "$@"
 }
